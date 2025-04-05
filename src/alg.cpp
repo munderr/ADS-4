b456a416ce35d1d32a4728b1683dfd80e646f385
munderr <mvyazov534@gmail.com>
2025-04-06 00:53:19 +0300

    Update alg.cpp (исправил с повторяющимися числами)

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,9 +18,18 @@ int countPairs2(int *arr, int len, int value) {
     while (left < right) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
-            count++;
-            left++;
-            right--;
+            int l = 1, r = 1;
+            while (left + l < right && arr[left + l] == arr[left]) l++;
+            while (right - r > left && arr[right - r] == arr[right]) r++;
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += (n * (n - 1)) / 2;
+                break;
+            } else {
+                count += l * r;
+                left += l;
+                right -= r;
+            }
         } else if (sum < value) {
             left++;
         } else {
@@ -30,23 +39,26 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 
-int binarySearch(int *arr, int left, int right, int target) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) return mid;
-        else if (arr[mid] < target) left = mid + 1;
-        else right = mid - 1;
-    }
-    return -1;
-}
-
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         int target = value - arr[i];
-        if (target < arr[i]) break; 
-        int index = binarySearch(arr, i + 1, len - 1, target);
-        if (index != -1) count++;
+        if (target < arr[i]) continue;
+
+        int left = i + 1, right = len - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        int start = left;
+        int countTarget = 0;
+        while (start < len && arr[start] == target) {
+            countTarget++;
+            start++;
+        }
+        count += countTarget;
     }
     return count;
 }
